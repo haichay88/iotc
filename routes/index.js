8c71002032e8cf63b18bb3ca09e862a29dcb99a7
Macintosh <macintosh@Macintosh.net.fpt>
2018-04-22 17:22:00 +0700

    log

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',layout:'layout'});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', layout: 'layout' });
 });
 
-router.get('/data/', function(req, res, next) {
-
+router.get('/data/', function (req, res, next) {
+  console.log("t1" + req.query.t1);
+  console.log("t2" + req.query.t2);
+  console.log("h1" + req.query.h1);
+  res.append("on1on2");
   //res.render('index', { title: 'Express',layout:'layout'});
 });
 
